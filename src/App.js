89479c177042b20882c3346909386d7aa8422ced
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-09-26 02:27:47 -0300

    Add Counter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Button from './components/Button';
+import Counter from './components/Counter';
 import clickCounterLogo from './images/click-counter-logo.png'
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <img className='click-counter-logo' src={clickCounterLogo} alt='Click Counter Logo' />
       </div>
       <div className='main-container'>
+        <Counter clicksNumber={5} />
         <Button text='Click' isClickButton={true} handleClick={addCounter} />
         <Button text='Reset' isClickButton={false} handleClick={resetCounter} />
       </div>
