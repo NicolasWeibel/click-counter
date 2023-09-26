369a483aaaf0aac099a370d136b9aa7e8687c19e
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-09-26 01:48:27 -0300

    Add click counter logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import clickCounterLogo from './images/click-counter-logo.png'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='click-counter-container-logo'>
+        <img className='click-counter-logo' src={clickCounterLogo} alt='Click Counter Logo' />
+      </div>
     </div>
   );
 }
